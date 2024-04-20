46796b861d155c0905026dce40f4b9ad734efe1b
Felipe <146666246+Lowfreqdrone@users.noreply.github.com>
2024-04-20 12:33:45 -0300

    beecrowd 1115

---
+++ beecrowd_1115.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+    scanf("%d %d",&x,&y);
+    while(x != 0 && y != 0)
+    {
+        if(x > 0 && y > 0){
+            printf("primeiro\n");
+        } else if(x < 0 && y > 0){
+            printf("segundo\n");
+        } else if(x < 0 && y < 0){
+            printf("terceiro\n");
+        } else if(x > 0 && y < 0){
+            printf("quarto\n");
+        } else{
+            break;
+        }
+        scanf("%d %d",&x,&y);
+    }
+    return 0;
+}
